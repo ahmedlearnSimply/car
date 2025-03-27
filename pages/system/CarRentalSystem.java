c05f79a160793c680f59fd40048e346acf10992d
mrrobot5-a <ahmedadel123422@gmail.com>
2025-03-27 20:54:28 +0200

    carRentalSystem

--- pages/system/CarRentalSystem.java
+++ pages/system/CarRentalSystem.java
@@ -6,11 +6,36 @@ import java.util.List;
 import car.Car;
 import customer.Customer;
 import customer.Reservation;
+import login.Admin;
 
 public class CarRentalSystem {
     private ArrayList<Car> carList = new ArrayList<>();
     private ArrayList<Customer> customers = new ArrayList<>();
     private ArrayList<Reservation> reservations = new ArrayList<>();
-
+    private Admin admin;
     // constructor
+
+    public CarRentalSystem(Admin admin) {
+        this.admin = admin;
+    }
+    // admin features
+    // ! admin login
+    // ! add car
+    // ! remove car
+    // ! view all booking
+    // ! update car info
+
+    // customer
+    // ! search ava cars
+    // ! bookcar
+    // ! cancel booking
+
+    // --load/ save data
+    // ! load cars
+    // ! load customers
+    // ! load reservations
+    // ! getCar list
+    // ! getCustomers
+    // ! getReservations
+
 }
