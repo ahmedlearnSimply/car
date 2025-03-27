f28283834ed6a1e92238d6cb169b33a9a98ea3a5
mrrobot5-a <ahmedadel123422@gmail.com>
2025-03-27 20:49:39 +0200

    commit

---
+++ pages/system/CarRentalSystem.java
@@ -0,0 +1,16 @@
+package system;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import car.Car;
+import customer.Customer;
+import customer.Reservation;
+
+public class CarRentalSystem {
+    private ArrayList<Car> carList = new ArrayList<>();
+    private ArrayList<Customer> customers = new ArrayList<>();
+    private ArrayList<Reservation> reservations = new ArrayList<>();
+
+    // constructor
+}
