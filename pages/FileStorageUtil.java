748d9fa470cf05ed6105dec640bf81a987678874
mrrobot5-a <ahmedadel123422@gmail.com>
2025-04-02 19:09:30 +0200

    commit

---
+++ pages/FileStorageUtil.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class FileStorageUtil {
+
+    public static List<Reservation> loadFromFile(String reservationFile) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loadFromFile'");
+    }
+
+}
